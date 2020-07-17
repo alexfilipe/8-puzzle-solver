93e24d35c4ef403fef4f9a1d433ddfdf6e72d560
alexfilipe <alex@afcbs.me>
2020-07-17 13:43:56 -0400

    add puzzle moves

--- puzzle.py
+++ puzzle.py
@@ -14,35 +14,115 @@ PUZZLE_GOAL = [
   [6, 7, 8]
 ]
 
+class IllegalMoveException(Exception):
+  """Move not allowed."""
+
 class Puzzle:
   """Represents an 8-puzzle."""
 
-  def __init__(self, puzzle_str=None):
-    self.puzzle_str = PUZZLE_GOAL_STR
-    self.puzzle_matrix = PUZZLE_GOAL
+  def __init__(self, puzzle_str=None, puzzle_matrix=None):
+    self.matrix = PUZZLE_GOAL
 
     if puzzle_str:
-      self.puzzle_str = puzzle_str
-      self.puzzle_matrix = Puzzle.str_to_matrix(puzzle_str)
+      self.matrix = Puzzle.str_to_matrix(puzzle_str)
+
+    elif puzzle_matrix:
+      self.matrix = puzzle_matrix
 
 
   def __str__(self):
-    """Pretty representation of the puzzle."""
+    """Pretty-print representation of the puzzle."""
     return "\n".join(
       " ".join(str(s) for s in r)
-      for r in self.puzzle_matrix
+      for r in self.matrix
     )
 
   def __hash__(self):
     """Hash representation of the puzzle (for dicts and sets)."""
-    return hash(self.puzzle_str)
+    return hash(self.as_string())
 
   def __eq__(self, other):
     """Compares two puzzles."""
     if not isinstance(other, type(self)):
       return NotImplementedError
 
-    return self.puzzle_str == other.puzzle_str
+    return self.as_string() == other.as_string()
+
+  def as_string(self):
+    return Puzzle.matrix_to_str(self.matrix)
+
+  def as_matrix(self):
+    return self.matrix
+
+
+  def is_solved(self):
+    """Returns True if this puzzle is solved."""
+    return self.as_string() == PUZZLE_GOAL_STR
+
+  def zero_index(self):
+    """Returns (i, j) representing the index of the zero element."""
+    i, j = 0, 0
+
+    while self.matrix[i][j] != 0 and i < 3 and j < 3:
+      if i < 2:
+        i += 1
+
+      if i == 2:
+        i = 0
+        j += 1
+
+    return i, j
+
+  def allowed_moves(self):
+    """Returns a set of allowed moves for the current puzzle."""
+    moves = set(["up", "right", "down", "left"])
+    i, j = self.zero_index()
+
+    if i == 0:
+      moves.remove("down")
+
+    if i == 2:
+      moves.remove("up")
+
+    if j == 0:
+      moves.remove("right")
+
+    if j == 2:
+      moves.remove("left")
+
+    return moves
+
+  def move(self, direction):
+    """Moves the puzzle in certain direction."""
+    if direction not in self.allowed_moves():
+      raise IllegalMoveException
+
+    i, j = self.zero_index()
+
+    if direction == "up":
+      self.__move_up(i, j)
+    elif direction == "right":
+      self.__move_right(i, j)
+    elif direction == "down":
+      self.__move_down(i, j)
+    elif direction == "left":
+      self.__move_left(i, j)
+
+  def __move_up(self, i, j):
+    self.matrix[i][j] = self.matrix[i+1][j]
+    self.matrix[i+1][j] = 0
+
+  def __move_right(self, i, j):
+    self.matrix[i][j] = self.matrix[i][j-1]
+    self.matrix[i][j-1] = 0
+
+  def __move_down(self, i, j):
+    self.matrix[i][j] = self.matrix[i-1][j]
+    self.matrix[i-1][j] = 0
+
+  def __move_left(self, i, j):
+    self.matrix[i][j] = self.matrix[i][j+1]
+    self.matrix[i][j+1] = 0
 
 
   @staticmethod
@@ -67,17 +147,17 @@ class Puzzle:
 
 
   @staticmethod
-  def matrix_to_str(puzzle_matrix):
+  def matrix_to_str(matrix):
     """Transforms a matrix representation of a puzzle into a string.
 
     Args:
-      puzzle_matrix (list of list): The 3x3 matrix representation of the
+      matrix (list of list): The 3x3 matrix representation of the
         puzzle.
     Returns:
       str: The string representation of the puzzle.
 
     """
     return "".join(
-      "".join([str(c) for c in r])
-      for r in puzzle_matrix
+      "".join(str(c) for c in r)
+      for r in matrix
     )
